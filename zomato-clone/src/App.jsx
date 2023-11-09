b0f376bace9cb83c67570e6a4083736e098916b4
Prapti <prapti@nagarro.com>
2023-11-09 13:58:46 +0530

    AccContainer and Accordance is added

--- zomato-clone/src/App.jsx
+++ zomato-clone/src/App.jsx
@@ -1,4 +1,6 @@
-import './App.css'
+import './App.scss'
+import AccContainer from './components/AccContainer/AccContainer.jsx';
+import Accordian from './components/Accordian/Accordian.jsx';
 import CTA from './components/CTA/CTA.jsx';
 import Card from './components/Card/Card.jsx';
 import Cities from './components/Cities/Cities.jsx';
@@ -16,6 +18,7 @@ function App() {
       <Collection />
       <Cities />
       <CTA />
+      <AccContainer />
       <Footer />
     </div>
   );
